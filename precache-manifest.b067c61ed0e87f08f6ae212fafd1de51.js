3c4c4385b95fc41fc50f2bb1ca798824595d2d25
iiska <iiska@users.noreply.github.com>
2019-12-15 09:30:11 +0000

    Automated deployment: Sun Dec 15 09:30:11 UTC 2019 312545f1bc1b809a82246a9284054152a6eb4077

---
+++ precache-manifest.b067c61ed0e87f08f6ae212fafd1de51.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "1d6cae86a83e2037b8f35b47ff5cc5e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "40953a21cfc843c1a686ee87bafc3e84",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "c038297de3c9e2b22b0e",
+    "url": "/static/css/main.90d6b2f9.chunk.css"
+  },
+  {
+    "revision": "c038297de3c9e2b22b0e",
+    "url": "/static/js/main.c038297d.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
